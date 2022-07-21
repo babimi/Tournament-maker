db132f2d0144dcaa4394abbac2f0abadf7a4065e
Babak Miraftab <58375015+babimi@users.noreply.github.com>
2022-07-21 14:30:08 -0600

    Update bm_2.py

--- bm_2.py
+++ bm_2.py
@@ -1,16 +1,17 @@
 def tournament_maker(txt, n):
     f = open(txt, "r")
     new = f.read()
-    t = new.split("\n")
+    s = new.split("\n")
+    t=[[int(y) for y in x] for x in [list(x) for x in s]]
     ff = []
     for x in t:
-        index_list = []
+        index_list = [0]
         final_list = []
         ffinal = []
         for i in range(n-1, n ^ 2-3*n+4, n-2):
             index_list.append(i)
         for i in range(n-1):
-            final_list.append(l[index_list[i]:index_list[i+1]])
+            final_list.append(x[index_list[i]:index_list[i+1]])
         for llst in final_list:
             lst = [0] * (n-len(llst))
             lst.extend(llst)
@@ -18,7 +19,7 @@ def tournament_maker(txt, n):
         if len(ffinal) != n:
             ffinal.append([0] * n)
         for i in range(len(ffinal)):
-            for j in range(i+1, n):
+            for j in range(i+1, n-1):
                 if ffinal[i][j] == 0:
                     ffinal[j][i] = 1
         ff.append(ffinal)
